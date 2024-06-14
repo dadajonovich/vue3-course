a21aa9ce0e2cab31c26b3fb0668ac67812761b1e
Norbaev <timnrbv@gmail.com>
2024-06-14 21:25:10 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Название" />
+      <input class="input" type="text" placeholder="Описание" />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'JavaScript', body: 'Описание поста 1' },
+        { id: 2, title: 'JavaScript', body: 'Описание поста 2' },
+        { id: 3, title: 'JavaScript', body: 'Описание поста 3' },
+        { id: 4, title: 'JavaScript', body: 'Описание поста 4' }
+      ]
+    };
+  },
+  methods: {
+    createPost() {}
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  padding: 20px;
+}
+
+.post {
+  padding: 15px;
+  border: solid 2px teal;
+  margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
