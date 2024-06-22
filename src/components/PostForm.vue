e93dee13fad96fe1b0fe257cea28f1ab69326641
Norbaev <timnrbv@gmail.com>
2024-06-22 23:42:48 +0500

    Пагинация через кнопки

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,8 +1,8 @@
 <template>
   <form @submit.prevent>
     <h4>Создание поста</h4>
-    <my-input v-model:value="post.title" type="text" placeholder="Название" />
-    <my-input v-model:value="post.body" type="text" placeholder="Описание" />
+    <my-input v-model="post.title" type="text" placeholder="Название" />
+    <my-input v-model="post.body" type="text" placeholder="Описание" />
     <my-button @click="createPost" style="align-self: flex-end; margin-top: 15px"
       >Создать</my-button
     >
